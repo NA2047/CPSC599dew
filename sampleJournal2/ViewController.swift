d2bc5991220c92b5358bf2d1253fed22adf5a7c9
raza-qazi <rmqazi@hotmail.com>
2017-11-16 18:18:57 -0700

    Created basic journal app.

--- sampleJournal2/ViewController.swift
+++ sampleJournal2/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet var toolbar: UIToolbar!
+    @IBOutlet weak var ViewTextField: UITextView!
+    
+    var startEditing: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ViewTextField.delegate = self;
+    }
+    
+    class JournalProperties {
+        var journalEntry: String = ""
+        var date: String = ""
+        var time: String = ""
+        
+        init(_ journal:String, _ date:String, _ time:String) {
+            self.journalEntry = journal
+            self.date = date
+            self.time = time
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if !self.startEditing {
+            textView.text = ""
+            textView.textColor = UIColor.black
+            self.startEditing = true
+        }
+    }
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        textView.inputAccessoryView = toolbar
+        return true
+    }
+    
+    @IBAction func saveJournalEntry(_ sender: UIButton) {
+        if self.ViewTextField.text == "" || self.ViewTextField.text == "Write here..." {
+            // Did not add anything new, yet.
+            print("No change in text field")
+        } else {
+            // Process new journal entry
+            
+            // Determine Date
+            let date = Date()
+            
+            let dateFormat = DateFormatter()
+            dateFormat.dateFormat = "MM/dd/yyyy"
+            let currentDate = dateFormat.string(from: date)
+            
+            let timeFormat = DateFormatter()
+            timeFormat.timeStyle = .short
+            let currentTime = timeFormat.string(from: date)
+            
+            // Store text entered into journal field
+            let enteredJournal = ViewTextField.text
+            
+            // New journal object
+            let newJournal = JournalProperties(enteredJournal!, currentDate, currentTime)
+            
+            print(newJournal.journalEntry)
+        }
+    }
+    
+    @IBAction func textfieldDoneButtonTapped(_ sender: UIBarButtonItem) {
+        self.view.endEditing(true)
+    }
 }
 
